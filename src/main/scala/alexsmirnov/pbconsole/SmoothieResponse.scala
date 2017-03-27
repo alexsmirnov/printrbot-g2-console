1da59f2f4294f3b4a1887fd1618140d51c39bacc
Alexander Smirnov <alexander.smirnov@mulesoft.com>
2017-03-27 11:22:47 -0700

    temp status parser

--- src/main/scala/alexsmirnov/pbconsole/SmoothieResponse.scala
+++ src/main/scala/alexsmirnov/pbconsole/SmoothieResponse.scala
@@ -3,6 +3,14 @@ package alexsmirnov.pbconsole
 object SmoothieResponse {
  val OK_RESPONSE = """^ok\s?(.*)$""".r
  val ERROR_RESPONSE = """^[eE]rror\s?(.*)$""".r
+ val extruderTempResponse = """T:(\d+\.?\d*)\s+/(\d+\.?\d*)\s+@(\d+)""".r
+ val valueMatchers:List[PartialFunction[String,Seq[ResponseValue]]] = List(
+     { case extruderTempResponse(current,target,output) => List(
+         ExtruderTemp(current.toFloat),
+         ExtruderTarget(target.toFloat),
+         ExtruderOutput(output.toInt))
+     }
+     )
  case class EmptyCommandResponse(rawLine: String,isError: Boolean = false) extends CommandResponse
  case class CommandResponseWithStatus(rawLine: String, values: List[ResponseValue], isError: Boolean = false)
  def apply(line: String): Response = line match {
