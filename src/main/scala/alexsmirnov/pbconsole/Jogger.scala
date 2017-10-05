3a50bbebf04e407e848f4f841de9d703209a6388
Alex Smirnov <alsmirnnov@gmail.com>
2017-10-05 12:34:15 -0700

    use 3d control for jogger

--- src/main/scala/alexsmirnov/pbconsole/Jogger.scala
+++ src/main/scala/alexsmirnov/pbconsole/Jogger.scala
@@ -22,6 +22,7 @@ import javafx.beans.property.DoubleProperty
 import scalafx.beans.binding.NumberExpression
 import scalafx.concurrent.Task
 import scalafx.util.Duration
+import alexsmirnov.scalafx.ArrowButton
 
 class Jogger(printer: PrinterModel, settings: Settings) {
 
@@ -34,68 +35,48 @@ class Jogger(printer: PrinterModel, settings: Settings) {
   def macroButton(label: String, commands: String*): Node = new Button(label) {
     minWidth = 45
     minHeight = 45
-    onAction = { ae: ActionEvent => printer.offer({ _ => commands.map(GCode(_)).iterator}, CommandSource.Monitor) }
+    onAction = { ae: ActionEvent => printer.offer({ _ => commands.map(GCode(_)).iterator }, CommandSource.Monitor) }
     disable <== printer.connected.not()
   }
   def move(axis: String, distance: Double, speed: Double = 1000) = printer.offer({ pos =>
-    val command = GCode("G0 " + axis + distance+"F"+speed)
-    if (pos.absolute) Iterator(GCode("G91"),command,GCode("G90")) else Iterator.single(command)
-  },CommandSource.Monitor)
-  
-  def moveX(distance: Double): Unit = move("X", distance)
-  def moveY(distance: Double): Unit = move("Y", distance)
-  def moveZ(distance: Double): Unit = move("Z", distance)
-  def moveE(distance: Double): Unit = printer.offer({ pos =>
-    val cmd = GCode("G0 E" + distance)
-    if (pos.absolute) Iterator(GCode("M83"),cmd,GCode("M83")) else Iterator(cmd)
-  },CommandSource.Monitor)
-  
+    val command = GCode("G0 " + axis + distance + "F" + speed)
+    if (pos.absolute) Iterator(GCode("G91"), command, GCode("G90")) else Iterator.single(command)
+  }, CommandSource.Monitor)
+
+  def moveExtruder(tool: String, distance: Double, speed: Double) = printer.offer({ pos =>
+    val cmd = GCode(s"G0 ${tool}${distance} F${speed}")
+    if (pos.extruderAbsolute) Iterator(GCode("M83"), cmd, GCode("M82")) else Iterator(cmd)
+  }, CommandSource.Monitor)
+
   val ssize = 2
-  
-  var joggerScheduler: Option[ScheduledService[Unit]] = None
-  def joggerButton(label: String, axis: String, step: NumberExpression,speed: NumberExpression): Node = new Button(label) {
-    minWidth = 45
-    minHeight = 45
-    disable <== printer.connected.not()
-    armed.onChange { 
-      joggerScheduler.foreach(_.cancel)
-      if(armed()) {
-        val ss = ScheduledService[Unit](Task[Unit](move(axis,step.toDouble,speed.toDouble)))
-        ss.period = Duration(settings.joggerInterval())
-        ss.start()
-        joggerScheduler = Some(ss)
+
+  @volatile
+  var joggerScheduler: Option[ScheduledService[Boolean]] = None
+  def joggerButtonArmed(dir: JoggerControl.Dir, armed: Boolean) {
+    joggerScheduler.foreach(_.cancel)
+    if (armed) {
+      val jogEstep = settings.jogEstep.toDouble
+      val jogEspeed = settings.jogEspeed.toDouble
+      val jogXYstep = settings.jogXYstep.toDouble
+      val jogXYspeed = settings.jogXYspeed.toDouble
+      val jogZstep = settings.jogZstep.toDouble
+      val jogZspeed = settings.jogZspeed.toDouble
+      def task = dir match {
+        case JoggerControl.EMinus | JoggerControl.EPlus => Task[Boolean](moveExtruder(dir.axis, jogEstep * dir.sign, jogEspeed))
+        case JoggerControl.ZMinus | JoggerControl.ZPlus => Task[Boolean](move(dir.axis, jogZstep * dir.sign, jogZspeed))
+        case _ => Task[Boolean](move(dir.axis, jogXYstep * dir.sign, jogXYspeed))
       }
+      val ss = ScheduledService[Boolean](task)
+      ss.period = Duration(settings.joggerInterval())
+      ss.start()
+      joggerScheduler = Some(ss)
     }
   }
-  val xyJogger = {
-    val grid = new GridPane()
-//    // X
-//    allSteps.foreach {
-//      case (step, index) =>
-//        grid.add(moveButton(step.toString(), moveX(step)), index + 1, steps.size + 1)
-//    }
-//    // Y
-//    allSteps.foreach {
-//      case (step, index) =>
-//        grid.add(moveButton(step.toString(), moveY(step)), steps.size + 1, steps.size * 2 - index + 1)
-//    }
-    grid.add(joggerButton("-X", "X", -settings.jogXYstep, settings.jogXYspeed),ssize,ssize+1)
-    grid.add(joggerButton("+X", "X", settings.jogXYstep, settings.jogXYspeed),ssize+2,ssize+1)
-    grid.add(joggerButton("+Y", "Y", settings.jogXYstep, settings.jogXYspeed),ssize+1,ssize)
-    grid.add(joggerButton("-Y", "Y", -settings.jogXYstep, settings.jogXYspeed),ssize+1,ssize+2)
-    grid.add(macroButton("H", "G28"), ssize + 1, ssize + 1)
-    grid.add(macroButton("Home X", "G28 X0"), 1, 1, 2, 1)
-    grid.add(macroButton("Home Y", "G28 Y0"), 1, ssize * 2 + 1, 2, 1)
-    grid.add(macroButton("Home XY", "G28 X0 Y0"), ssize * 2, 1, 2, 1)
-    grid.add(macroButton("Motors Off", "M84"), ssize * 2, ssize * 2 + 1, 2, 1)
-    grid.alignment = Pos.Center
-    // visual effects
-    grid.hgap = 5.0
-    grid.vgap = 5.0
-    grid.padding = Insets(5)
-    grid
-  }
+  val xyJogger = new JoggerControl()
 
+  xyJogger.disable <== printer.connected.not()
+  xyJogger.onAxisArmed(joggerButtonArmed _)
+  xyJogger.onHomeAction(printer.offer({ _ => Iterator(GCode("G28")) }, CommandSource.Monitor))
   //  val macros = ObservableBuffer("Bed Level" -> Seq("G38.4",
   //    "M400",
   //    "G32",
@@ -111,32 +92,31 @@ class Jogger(printer: PrinterModel, settings: Settings) {
     new Button() {
       margin = Insets(5)
       text <== m.nameProperty
-      onAction = { ae: ActionEvent => printer.offer({ _ => Macro.prepare(m.content, settings).map(GCode(_))}, CommandSource.Monitor) }
+      onAction = { ae: ActionEvent => printer.offer({ _ => Macro.prepare(m.content, settings).map(GCode(_)) }, CommandSource.Monitor) }
       disable <== printer.connected.not()
     }.delegate
   }
   val node: Node = new BorderPane {
     padding = Insets(10)
-    center = xyJogger
+    center = xyJogger.node
     right = new VBox {
       spacing = 5
       padding = Insets(5)
       alignment = Pos.Center
-      children =   joggerButton("Z", "Z", settings.jogZstep, settings.jogZspeed) :: 
-                   joggerButton("-Z", "Z", -settings.jogZstep, settings.jogZspeed) :: 
-                   Nil
+      children = List(
+        new ArrowButton("-E", JoggerControl.arrowDim, ArrowButton.Up) {
+          disable <== printer.connected.not()
+          styleClass = JoggerControl.ArrowBtn
+          armed.onChange(joggerButtonArmed(JoggerControl.EMinus, armed()))
+        },
+        new ArrowButton("+E", JoggerControl.arrowDim, ArrowButton.Down) {
+          disable <== printer.connected.not()
+          styleClass = JoggerControl.ArrowBtn
+          armed.onChange(joggerButtonArmed(JoggerControl.EPlus, armed()))
+        })
     }
     bottom = new VBox {
-      children = List(new HBox {
-        vgrow = Priority.NEVER
-        alignment = Pos.Center
-        spacing = 5
-        padding = Insets(5)
-      children =   joggerButton("E", "E", settings.jogEstep, settings.jogEspeed) :: 
-                   joggerButton("-E", "E", -settings.jogEstep, settings.jogEspeed) :: 
-                   Nil
-      },
-        macros)
+      children = List(macros)
     }
   }
 }
--- src/main/scala/alexsmirnov/pbconsole/JoggerControl.scala
+++ src/main/scala/alexsmirnov/pbconsole/JoggerControl.scala
@@ -28,47 +28,67 @@ import scalafx.scene.layout.HBox
 import scalafx.scene.layout.VBox
 import scalafx.geometry.Pos
 import scalafx.scene.shape.Circle
+import scalafx.event.ActionEvent
+import scalafx.beans.property.BooleanProperty
 
-class JoggerControl {
+object JoggerControl {
   val h = 150f
   val s = 300f
-  val arrowDim = ArrowButton.Dimensions(50,40,30,80)
-  val ArrowBtn = List("arrow","button")
+  val arrowDim = ArrowButton.Dimensions(50, 40, 30, 80)
+  val ArrowBtn = List("arrow", "button")
   val xAxis = "xAxis" :: ArrowBtn
   val yAxis = "yAxis" :: ArrowBtn
   val zAxis = "zAxis" :: ArrowBtn
-  val node: Node = {
-    val home = new Button {
-          maxWidth = 50
-          maxHeight = 50
-          shape = Circle(25)
-          graphic = new ImageView {
-            image = new Image(this.getClass.getResourceAsStream("/images/3dHome.png"),50,50,true,true)
-            margin = Insets(5)
-          }
+  sealed abstract class Dir(val axis: String,val sign: Int)
+  case object XPlus extends Dir("X",1)
+  case object XMinus extends Dir("X",-1)
+  case object YPlus extends Dir("Y",1)
+  case object YMinus extends Dir("Y",-1)
+  case object ZPlus extends Dir("Z",1)
+  case object ZMinus extends Dir("Z",-1)
+  case object EPlus extends Dir("E",1)
+  case object EMinus extends Dir("E",-1)
+}
+class JoggerControl { self =>
+  import JoggerControl._
+  val home = new Button {
+    maxWidth = 50
+    maxHeight = 50
+    shape = Circle(25)
+    graphic = new ImageView {
+      image = new Image(this.getClass.getResourceAsStream("/images/3dHome.png"), 50, 50, true, true)
+      margin = Insets(5)
     }
+  }
+  def onHomeAction(f: => Unit) {
+    home.onAction = { ae: ActionEvent => f }
+  }
+  val disable = BooleanProperty(false)
+  // Axis actions
+  private[this] var axisListeners: List[(Dir,Boolean) => Unit] = Nil
+  def onAxisArmed(f: (Dir,Boolean) => Unit) = { axisListeners = f :: axisListeners }
+  private def axisArmedChanged(dir: Dir,armed: Boolean) = axisListeners.foreach(_(dir,armed))
+  val node: Node = {
     new HBox { root =>
+      disable <== self.disable
       children = List(
-          new VBox(60) {
-            alignment = Pos.CenterRight
-            children = List(
-                new ArrowButton("+Y",arrowDim,ArrowButton.Left){rotate = 30;styleClass=yAxis}, 
-                new ArrowButton("-X",arrowDim,ArrowButton.Left){rotate = -30;styleClass=xAxis}
-                )
-          },
-          new VBox(20) {
-            alignment = Pos.Center
-            children = List(new ArrowButton("+Z",arrowDim,ArrowButton.Up){styleClass=zAxis},
-                home,
-                new ArrowButton("-Z",arrowDim,ArrowButton.Down){styleClass=zAxis}
-                )
-          },
-          new VBox(60) {
-            alignment = Pos.CenterLeft
-            children = List(new ArrowButton("+X",arrowDim,ArrowButton.Right){rotate = -30;styleClass=xAxis},
-                new ArrowButton("-Y",arrowDim,ArrowButton.Right){rotate = 30;styleClass=yAxis})
-          }
-          )
+        new VBox(60) {
+          alignment = Pos.CenterRight
+          children = List(
+            new ArrowButton("+Y", arrowDim, ArrowButton.Left) { rotate = 30; styleClass = yAxis; armed.onChange(axisArmedChanged(YPlus, armed())) },
+            new ArrowButton("-X", arrowDim, ArrowButton.Left) { rotate = -30; styleClass = xAxis; armed.onChange(axisArmedChanged(XMinus, armed()))  })
+        },
+        new VBox(20) {
+          alignment = Pos.Center
+          children = List(new ArrowButton("+Z", arrowDim, ArrowButton.Up) { styleClass = zAxis; armed.onChange(axisArmedChanged(ZPlus, armed()))  },
+            home,
+            new ArrowButton("-Z", arrowDim, ArrowButton.Down) { styleClass = zAxis; armed.onChange(axisArmedChanged(ZMinus, armed()))  })
+        },
+        new VBox(60) {
+          alignment = Pos.CenterLeft
+          children = List(new ArrowButton("+X", arrowDim, ArrowButton.Right) { rotate = -30; styleClass = xAxis; armed.onChange(axisArmedChanged(XPlus, armed()))  },
+            new ArrowButton("-Y", arrowDim, ArrowButton.Right) { rotate = 30; styleClass = yAxis; armed.onChange(axisArmedChanged(YMinus, armed()))  })
+        })
     }
   }
 }
